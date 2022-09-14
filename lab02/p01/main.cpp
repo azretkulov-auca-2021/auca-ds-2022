6237efb56b491187d9b5e2abbd71ebfac10587d4
Adis Azretkulov <azretkulov_a@auca.kg>
2022-09-14 12:51:53 +0200

    lab02

---
+++ lab02/p01/main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+template <typename C>
+int sz(const C &c) { return static_cast<int>(c.size()); }
+
+using namespace std;
+
+int solve(int a, int b) {
+    if (a > b) {
+        swap(a, b);
+    }
+
+int MaxLength = 0;
+int nSteps = 0;
+for(int i = a; i <= b; ++i)
+{
+   if() 
+}
+}
+
+
+int main()
+{
+    for (int a, b; cin >> a >> b;)
+    iostream::sync_with_stdio(false);
+}
