8dc78d7b5182958f1d3bc71dab92f3f851ca9933
Adis Azretkulov <azretkulov_a@auca.kg>
2022-10-05 12:55:30 +0200

    uva 12015 not solved yet

---
+++ lab02/p15/main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+template <typename C>
+int sz(const C &c) { return static_cast<int>(c.size()); }
+
+using namespace std;
+
+int main()
+{
+    iostream::sync_with_stdio(false);
+
+    int t;
+    cin >> t;
+
+    for (int i = 1; i <= t; i++)
+    {
+        vector<string> v1;
+        vector<int> v2;
+        int max = 0;
+
+        for (int j = 0; j < 10; j++) {}
+
+    }
+    
+}
