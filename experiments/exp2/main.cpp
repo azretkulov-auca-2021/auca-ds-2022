c75b768343129289621af79bfb617b9be3d68dad
Adis Azretkulov <azretkulov_a@auca.kg>
2022-10-05 12:18:50 +0200

    exp 2 vector

---
+++ experiments/exp2/main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+template <typename C>
+int sz(const C &c) { return static_cast<int>(c.size()); }
+
+using namespace std;
+
+int main()
+{
+    iostream::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+
+    for (auto &e : v) {
+        cin >> e;
+    }
+
+    for (auto e : v) {
+        cout << " " << e << endl;
+    }
+}
