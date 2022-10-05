d60c6d996c7f2d41443ef39b8dedd31fb1fb5d8d
Adis Azretkulov <azretkulov_a@auca.kg>
2022-10-05 11:40:05 +0200

    lab 2 problem 4  not solved yet

---
+++ lab02/p04/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+template <typename C>
+int sz(const C &c) { return static_cast<int>(c.size()); }
+
+using namespace std;
+
+int main()
+{
+    int t, n, s;
+    cin >> t;
+
+    iostream::sync_with_stdio(false);
+}
