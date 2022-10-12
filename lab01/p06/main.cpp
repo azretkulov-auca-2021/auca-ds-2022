0e59bcbfdd853a05827075cab7e265c843c04319
Adis Azretkulov <azretkulov_a@auca.kg>
2022-10-12 12:53:58 +0200

    lab 1 problem 6 in progress

---
+++ lab01/p06/main.cpp
@@ -0,0 +1,52 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "../../doctest/doctest.h"
+
+#include <vector> 
+
+using namespace std;
+
+TEST_CASE("Deafult constructor") {
+    vector<int> v;
+
+    REQUIRE(v.empty());
+    REQUIRE(v.size() == 0);
+}
+
+TEST_CASE("Copy constructor") {
+    vector<int> v = {1, 2, 3, 4, 5};
+
+    //TODO
+}
+
+TEST_CASE("push back") {
+    vector<int> v;
+
+    //TODO
+}
+
+TEST_CASE("back") {
+    vector<int> v = {1, 2, 3};
+
+    REQUIRE(v.back() == 3);
+    v.back() = 100;
+
+    REQUIRE(v.back() == 100);
+}
+
+TEST_CASE("front") {
+    vector<int> v = {1, 2, 3};
+
+    REQUIRE(v.front() == 1);
+    v.front() = 100;
+
+    REQUIRE(v.front() == 100);
+}
+
+TEST_CASE("[]") {
+    vector<int> v = {1, 2, 3};
+
+    REQUIRE(v[0] == 1);
+    v.assign(1, 100);
+
+    REQUIRE(v[0] == 100);
+}
