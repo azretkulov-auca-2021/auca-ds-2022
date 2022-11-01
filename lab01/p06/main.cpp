ba7def33463d0fa0f674035f498c47181cebdc05
Adis Azretkulov <azretkulov_a@auca.kg>
2022-11-01 15:10:04 +0100

    lab1 p06 fixed iterators

--- lab01/p06/main.cpp
+++ lab01/p06/main.cpp
@@ -1,17 +1,20 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "../../doctest/doctest.h"
 
-#include <vector> 
+#include<vector>
 
 using namespace std;
 
 template <typename C>
-string containerToStr(const C &c) {
+string containerToStr(const C &c)
+{
     ostringstream sout;
     bool isFirst = true;
     sout << "{";
-    for (const auto &e : c) {
-        if (!isFirst) {
+    for (const auto &e : c)
+    {
+        if (!isFirst)
+        {
             sout << ", ";
         }
         sout << e;
@@ -22,7 +25,8 @@ string containerToStr(const C &c) {
     return sout.str();
 }
 
-TEST_CASE("Deafult constructor") {
+TEST_CASE("default constructor")
+{
     vector<int> v;
 
     REQUIRE(v.empty());
@@ -31,9 +35,9 @@ TEST_CASE("Deafult constructor") {
 
 TEST_CASE("constructor with size")
 {
-    vector<int> v(5);
+    vector<int> v(10);
 
-    REQUIRE(v.size() == 5);
+    REQUIRE(v.size() == 10);
 
     for (auto i : v)
     {
@@ -43,16 +47,18 @@ TEST_CASE("constructor with size")
 
 TEST_CASE("constructor with size and initial value")
 {
-    vector<int> v(5, 2);
+    vector<int> v(10, 1);
 
-    REQUIRE(v.size() == 5);
+    REQUIRE(v.size() == 10);
 
     for (auto i : v)
     {
-        REQUIRE(i == 2);
+        REQUIRE(i == 1);
     }
 }
-TEST_CASE("Copy constructor") {
+
+TEST_CASE("copy constructor")
+{
     vector<int> v = {1, 2, 3, 4, 5};
 
     vector<int> v2 = v;
@@ -60,27 +66,27 @@ TEST_CASE("Copy constructor") {
     REQUIRE(v2.size() == 5);
     REQUIRE(containerToStr(v2) == "{1, 2, 3, 4, 5}");
 
-    v[1] = 666;
-    v2[3] = 21;
+    v[0] = 1000;
+    v2[4] = 42;
 
-    REQUIRE(containerToStr(v) == "{1, 666, 3, 4, 5}");
-    REQUIRE(containerToStr(v2) == "{1, 2, 3, 21, 5}");
+    REQUIRE(containerToStr(v) == "{1000, 2, 3, 4, 5}");
+    REQUIRE(containerToStr(v2) == "{1, 2, 3, 4, 42}");
 }
 
 TEST_CASE("assignment operator")
 {
-    vector<int> v = {1, 2, 3, 4, 5};
-    vector<int> c = {5, 6, 7, 8, 9};
+    vector<int> v = {1, 2, 3};
+    vector<int> c = {3, 4, 5};
 
     c = v;
 
     REQUIRE(c[0] == 1);
 
-    v[1] = 21;
-    c[4] = 99;
+    v[2] = 34;
+    c[0] = 100;
 
-    REQUIRE(containerToStr(v) == "{1, 21, 3, 4, 5}");
-    REQUIRE(containerToStr(c) == "{1, 2, 3, 4, 99}");
+    REQUIRE(containerToStr(v) == "{1, 2, 34}");
+    REQUIRE(containerToStr(c) == "{100, 2, 3}");
 
     v = vector<int>();
 
@@ -107,42 +113,12 @@ TEST_CASE("move assignment")
     REQUIRE(containerToStr(v2) == "{1, 2, 3}");
 }
 
-TEST_CASE("push back") {
-    vector<int> v;
-
-    v.push_back(1);
-    v.push_back(2);
-    v.push_back(3);
-    v.push_back(4);
-    v.push_back(5);
-
-    REQUIRE(v.size() == 5);
-    REQUIRE(containerToStr(v) == "{1, 2, 3, 4, 5}");
-    REQUIRE(v.capacity() == 8);
-}
-
-TEST_CASE("back") {
-    vector<int> v = {1, 2, 3};
-
-    REQUIRE(v.back() == 3);
-    v.back() = 100;
-
-    REQUIRE(v.back() == 100);
-}
-
-TEST_CASE("front") {
-    vector<int> v = {1, 2, 3};
-
-    REQUIRE(v.front() == 1);
-    v.front() = 100;
-
-    REQUIRE(v.front() == 100);
-}
-
-TEST_CASE("[]") {
+TEST_CASE("[]")
+{
     vector<int> v = {1, 2, 3};
 
     REQUIRE(v[0] == 1);
+
     v.assign(1, 100);
 
     REQUIRE(v[0] == 100);
@@ -154,7 +130,22 @@ TEST_CASE("at()")
 
     REQUIRE(v.at(1) == 2);
 
-    REQUIRE_THROWS_AS(v.at(4), out_of_range);
+    REQUIRE_THROWS_AS(v.at(5), out_of_range);
+}
+
+TEST_CASE("push back")
+{
+    vector<int> v;
+
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(4);
+    v.push_back(5);
+
+    REQUIRE(v.size() == 5);
+    REQUIRE(containerToStr(v) == "{1, 2, 3, 4, 5}");
+    REQUIRE(v.capacity() == 8);
 }
 
 TEST_CASE("pop_back")
@@ -172,7 +163,7 @@ TEST_CASE("pop_back")
 
 TEST_CASE("auto element : container")
 {
-    vector<int> v = {1, 2, 3, 4};
+    vector<int> v = {1, 2, 3};
     int t = 0;
 
     for (auto i : v)
@@ -180,7 +171,84 @@ TEST_CASE("auto element : container")
         t += i;
     }
 
-    REQUIRE(t == 10);
+    REQUIRE(t == 6);
+}
+
+TEST_CASE("back()")
+{
+    vector<int> v = {1, 2, 3};
+
+    REQUIRE(v.back() == 3);
+
+    v.back() = 100;
+
+    REQUIRE(v.back() == 100);
+}
+
+TEST_CASE("front()")
+{
+    vector<int> v = {1, 2, 3};
+
+    REQUIRE(v.front() == 1);
+
+    v.front() = 100;
+
+    REQUIRE(v.front() == 100);
+}
+
+class Student
+{
+    string mName;
+    double mGPA;
+
+public:
+    Student(const string &name, double gpa)
+        : mName(name), mGPA(gpa)
+        {
+        }
+    const string &name() const{
+        return mName;
+    }
+};
+
+TEST_CASE("iterators")
+{
+    vector<int> v = {4, 2, 1, 2, 5, 4};
+    vector<Student> s = {{"StudentA", 4.00}, {"StudentB", 1.6}};
+
+    SUBCASE("* dereference")
+    {
+        auto it = v.begin();
+        REQUIRE(*it == 4);
+        *it = 10;
+        REQUIRE(v.front() == 10);
+    }
+
+    SUBCASE("->")
+    {
+        auto it = s.begin();
+        REQUIRE((*it).name() == "StudentA");
+        REQUIRE(it->name() == "StudentA");
+    }
+
+    SUBCASE("+")
+    {
+        auto it = v.begin();
+        REQUIRE(*it == 4);
+        REQUIRE(*(it + 4) == 5);
+        it += 3;
+        REQUIRE(*it == 2);
+    }
+
+    SUBCASE("it1 - it2")
+    {
+        auto it1 = v.begin();
+        auto it2 =  it1 + 2;
+        REQUIRE(it2 - it1 == 2);
+        auto it3 = v.begin() + 4;
+        int index = it3 - v.begin();
+        REQUIRE(v[index] == 5);
+    }
 }
 
 TEST_CASE("insert(it, value), insert(it, beg, end")
@@ -243,7 +311,7 @@ TEST_CASE("sort")
 
 TEST_CASE("binary_search")
 {
-    vector<int> v(5, 10);
+    vector<int> v = {10, 10, 10, 10, 10};
 
     REQUIRE(binary_search(v.begin(), v.end(), 10) == true);
 }
@@ -261,48 +329,3 @@ TEST_CASE("max_element")
 
     REQUIRE(*max_element(v.begin(), v.end()) == 7);
 }
-
-class Student 
-{
-    string mName;
-    double mGPA;
-
-public: 
-        Student(const string &name, double gpa)
-        : mName(name), mGPA(gpa)
-        {
-        }
-    const string &name() const{
-        return mName;
-    }
-};
-
-TEST_CASE("iterator") {
-    vector<int> v = {4, 2, 1, 5, 2};
-    vector<Student> s = {{"StudentA", 4.00}, {"StudentB", 1.6}};
-
-    SUBCASE("* dereference") 
-    {
-        auto it = v.begin();
-        REQUIRE(*it == 4);
-        
-        *it = 10;
-        REQUIRE(v.front() == 10);
-    }
-
-    SUBCASE("->")
-    {
-        auto it = s.begin();
-        REQUIRE((*it).name() == "StudentA");
-        REQUIRE(it->name() == "StudentA");
-    }
-
-    SUBCASE("+")
-    {
-        auto it = v.begin();
-        REQUIRE(*it == 4);
-        REQUIRE(*(it + 4) == 5);
-        it += 3;
-        REQUIRE(*it == 2);    }
-    
-    }
