4dca139dc1a905a4312ae26f94189d9fb1eb22c3
Adis Azretkulov <azretkulov_a@auca.kg>
2022-12-06 06:06:53 +0100

    algol hpp file

---
+++ lab06/main/main.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+// #include "../../"
+
+using namespace std;
+
+struct Student
+{
+    string mName;
+    double mGpa;
+    Student(const string &name, double gpa)
+        : mName(name), mGpa(gpa)
+    {
+    }
+};
+
+void p0101()
+{
+    int a[] = {3, 1, 20, 4, 7, 0, 5};
+}
+
+int main()
+{
+    p0101();
+    // p0102();
+}
