ead4b04f5f23ef8e136f20efca62f0f5b7db7cf0
Adis Azretkulov <azretkulov_a@auca.kg>
2022-12-16 14:25:03 +0100

    lab 4 files

--- lab04/p01/main.c
+++ lab04/p01/main.c
@@ -7,5 +7,18 @@ int main(void)
     int *p;
 
     x = 1;
-    p = &x;
+    p = &x; // address of operator
+
+    printf("x = %d\n", x);
+    printf("address of x = %p\n", (void *)p);
+    printf("x = %d\n", *p); // dereference operator
+
+    *p = 2;
+
+    printf("x = %d\n", x);
+    printf("x = %d\n", *p);
+
+    y = *p * *p;
+
+    printf("y = %d\n", y);
 }
